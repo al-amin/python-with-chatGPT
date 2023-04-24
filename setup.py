2ace17780a6cddd69651981759f4a546eda6a4d7
AL AMIN <encrypt.h@gmail.com>
2023-04-24 22:54:26 +0200

    comments added
    comments added

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 import keyring
-keyring.set_password("openAI", "API-KEY", "API-KEY-SECRET")
+keyring.set_password("openAI", "API-KEY", "API-KEY-SECRET") # replace the API-KEY-SECRET with your open AI secret API-KEY
 print(keyring.get_password("openAI", "API-KEY"))
